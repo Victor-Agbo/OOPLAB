6c81f83f67c12a990d316c82da832b43a0b034e4
Victor-Agbo <victoragbo72@gmail.com>
2023-04-07 14:20:03 +0530

    Wrote the Test class

--- src/LAB7/Para.java
+++ src/LAB7/Para.java
@@ -1,7 +1,25 @@
 package LAB7;
 
 public class Para {
-    public Para(){
+    private int id;
+    private String name;
 
+    public Para(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public void printData() {
+        System.out.println();
+        System.out.println("Id: " + id);
+        System.out.println("Name: " + name);
+    }
+
+    public static void main(String[] args) {
+        Para obj1 = new Para(1, "Object 1");
+        Para obj2 = new Para(2, "Object 2");
+        
+        obj1.printData();
+        obj2.printData();
     }
 }
